df7a5103ead82f3a714c08c96bf8b126d05d92dd
grubby-github <re_grubby@hotmail.com>
2019-11-06 11:40:57 +0800

    更新配置
    更新配置

--- cloud-eureka/src/main/java/cn/gaoz/example/CloudEurekaApplication.java
+++ cloud-eureka/src/main/java/cn/gaoz/example/CloudEurekaApplication.java
@@ -1,6 +1,11 @@
 package cn.gaoz.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudEurekaApplication {
 
     public static void main(String[] args) {
