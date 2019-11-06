e7fa4b72974045280a17774093663ba8e8f4046e
grubby-github <re_grubby@hotmail.com>
2019-11-06 11:34:43 +0800

    同步版本
    同步版本

---
+++ cloud-eureka/src/main/java/cn/gaoz/example/CloudEurekaApplication.java
@@ -0,0 +1,9 @@
+package cn.gaoz.example;
+
+@SpringBootApplication
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
