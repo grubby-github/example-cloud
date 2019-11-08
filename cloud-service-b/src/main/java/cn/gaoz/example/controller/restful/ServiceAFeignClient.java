fe84e89e5514b7afb30e4857089f1f0ca9727ba3
grubby-github <re_grubby@hotmail.com>
2019-11-08 15:44:49 +0800

    修复405错误
    修复405错误

--- cloud-service-b/src/main/java/cn/gaoz/example/controller/restful/ServiceAFeignClient.java
+++ cloud-service-b/src/main/java/cn/gaoz/example/controller/restful/ServiceAFeignClient.java
@@ -2,9 +2,11 @@ package cn.gaoz.example.controller.restful;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("cloud-service-a")
 public interface ServiceAFeignClient {
     @GetMapping("/rest/testa")
-    String testA(String req);
+    String testA(@RequestParam String req);
 }
