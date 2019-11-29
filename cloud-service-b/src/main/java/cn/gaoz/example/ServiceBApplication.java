71ee2b355874aeda727e9caf54d0bbdf7e29c823
grubby-github <re_grubby@hotmail.com>
2019-11-29 19:54:00 +0800

    同步最新版本
    同步最新版本

--- cloud-service-b/src/main/java/cn/gaoz/example/ServiceBApplication.java
+++ cloud-service-b/src/main/java/cn/gaoz/example/ServiceBApplication.java
@@ -2,7 +2,9 @@ package cn.gaoz.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,9 +16,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
-public class ServiceBApplication {
+public class ServiceBApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceBApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServiceBApplication.class);
     }
 }
